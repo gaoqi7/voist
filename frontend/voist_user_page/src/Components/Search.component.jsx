318c2a5cba897d05bbd2943bddeea58a989dea12
Rick Gao <ricky@Qis-MacBook-Pro.local>
2020-02-13 16:48:34 -0800

    finish the user search function

--- frontend/voist_user_page/src/Components/Search.component.jsx
+++ frontend/voist_user_page/src/Components/Search.component.jsx
@@ -1,53 +1,52 @@
 import React, { useState, useEffect } from "react";
 import { database } from "../App";
-import { useObjectVal } from "react-firebase-hooks/database";
-// import { TrackQueryResult } from "./TrackQueryResult/TrackQueryRusult.component";
 export const Search = () => {
   const [query, setQuery] = useState("");
-  const [atoken] = useObjectVal(database.ref("token/access_token"));
-  const [tqResult, set_tqResult] = useState([]);
-  useEffect(() => console.log(atoken), [atoken]);
   useEffect(() => {
-    console.log(query);
-    const option = {
-      method: "get",
-      headers: { Authorization: `Bearer ${atoken}` }
-    };
-    const fetchSearchResult = async () => {
-      if (query.length > 5) {
-        const response = await fetch(
-          `https://api.spotify.com/v1/search?q=${query}&type=track&limit=7&offset=5`,
-          option
+    if (query.length >= 3) {
+      let accessToken;
+      let tokenType;
+      const fetchToken = async () => {
+        const res = await database.ref("token").once("value");
+        accessToken = await res.val().access_token;
+        tokenType = await res.val().token_type;
+        console.log(accessToken);
+        console.log(tokenType);
+      };
+
+      const querySearch = async () => {
+        let opt = {
+          method: "GET",
+          headers: {
+            Authorization: `${tokenType} ${accessToken}`
+          }
+        };
+        const res = await fetch(
+          `https://api.spotify.com/v1/search?q=${query}&type=track`,
+          opt
         );
-        const resJson = await response.json();
-        console.log(resJson.tracks.items);
-        set_tqResult(resJson.tracks.items);
-      }
-    };
-    fetchSearchResult();
+        const resJson = await res.json();
+        console.log(resJson);
+      };
+
+      const queryAction = async () => {
+        await fetchToken();
+        await querySearch();
+      };
+      queryAction();
+    }
   }, [query]);
   return (
-    <>
-      <form>
-        <label>
-          Search:
-          <input
-            type="text"
-            value={query}
-            onChange={event => setQuery(event.target.value)}
-            name="name"
-          />
-        </label>
-      </form>
-      <ul>
-        {tqResult.length > 0 &&
-          query.length > 5 &&
-          tqResult.map(e => (
-            <li key={e.id} id={e.id}>
-              {e.name}
-            </li>
-          ))}
-      </ul>
-    </>
+    <form>
+      <label>
+        Search:
+        <input
+          type="text"
+          value={query}
+          onChange={e => setQuery(e.target.value)}
+          name="name"
+        />
+      </label>
+    </form>
   );
 };
